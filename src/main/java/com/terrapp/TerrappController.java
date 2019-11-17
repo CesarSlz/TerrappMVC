5b511399ef5e994a901bfceb72b8b2bb10e867f4
Vit456 <robertoviteruiz@hotmail.com>
2019-11-17 01:59:33 -0600

    Vistas del MVC generadas

---
+++ src/main/java/com/terrapp/TerrappController.java
@@ -0,0 +1,27 @@
+package com.terrapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TerrappController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView getData() {
+
+		ModelAndView model = new ModelAndView("index");
+
+		return model;
+	}
+
+	@RequestMapping(value = "/property", method = RequestMethod.GET)
+	public ModelAndView getData2() {
+
+		ModelAndView model = new ModelAndView("property");
+
+		return model;
+
+	}
+}
